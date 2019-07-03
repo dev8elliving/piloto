efcbe5ff1f023db4f3177df358fcf769333eb3fc
dev8elliving <52512657+dev8elliving@users.noreply.github.com>
2019-07-03 19:10:05 -0400

    Subida del Proyecto
    Esto es un proyecto de prueba ...

---
+++ icalma2/exequiel/elige-tu-depa.php
@@ -0,0 +1,341 @@
+    <!-- SLIDE PROYECTOS -->
+    <section id="elige_tu_depa">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="todo_el">
+              <h2 class="elige">Elige tu departamento</h2>
+               <div class="elselect">
+                <span class="purp_select">
+                  <img src="assets/images/botarrow.png" alt="botarrow">
+                </span>   
+                <select onclick="ga('send', 'event', 'select', 'click', 'select_elige_tu_depa');" name="select_txt" class="select_propio">
+                  <option value="depa_a">DEPARTAMENTO TIPO A - 1 DORMITORIO + 1 BAÑO</option>
+                  <option value="depa_b1">DEPARTAMENTO TIPO B1 - 2 DORMITORIOS + 1 BAÑO</option>
+                  <option value="depa_b2">DEPARTAMENTO TIPO B2 - 2 DORMITORIOS + 1 BAÑO</option>
+                  <option value="depa_c1">DEPARTAMENTO TIPO C1 - 2 DORMITORIOS + 2 BAÑOS</option>
+                  <option value="depa_c3">DEPARTAMENTO TIPO C3 - 2 DORMITORIOS + 2 BAÑOS</option>
+                  <option value="depa_d">DEPARTAMENTO TIPO D (TIPO MARIPOSA) - 2x (2 DORMITORIOS + 2 BAÑOS)</option>
+                  <option value="depa_e">DEPARTAMENTO TIPO E1 - 3 DORMITORIOS + 2 BAÑOS</option>
+                </select>     
+              </div>             
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- PROYECTO 1 -->
+      <div class="proyecto pro1">
+        <div class="container">
+          <div class="row">
+            <div class="col"> 
+              <div class="row"> 
+                <div class="col-lg-4">
+                  <div class="descripcion">
+                    <p class="purp-text">DEPARTAMENTO TIPO A</p>
+                    <p class="desde">DESDE <span>2.329 UF</span></p>
+                    <p class="pro-desc">1 DORMITORIO + 1 BAÑO</p>
+                    <p class="orientacion">Orientación Nororiente</p>
+                    <p class="p-suma">
+                      <span>Superficie interior</span>
+                      <span class="text-right">31,21 m<sup>2</sup></span>
+                    </p>
+                    <p class="p-suma">
+                      <span>Superficie terraza</span>
+                      <span class="text-right">5,19 m<sup>2</sup></span>
+                    </p>
+                    <div class="hr-purp"></div>
+                    <p class="p-suma total">
+                      <span class="stotal">Superficie total</span>
+                      <span class="text-right">36,4 m<sup>2</sup></span>
+                    </p>
+                    <div class="btns_promo">               
+                      <div class="btndepa blue">
+                        <a onclick="ga('send', 'event', 'btn', 'click', 'btn_mas_info_proyecto');" href="#modal_formulario" data-toggle="modal">
+                         Solicita más información    
+                        </a>                 
+                      </div>                                                
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-8">
+                  <div class="contplano">
+                   <img class="plano" src="assets/images/proyectos/tipo_a.jpg" alt="ICALMA - DEPARTAMENTO TIPO A">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- FIN PROYECTO 1 -->
+      <!-- PROYECTO 2 -->
+      <div class="proyecto pro2 proOff">
+        <div class="container">
+          <div class="row">
+            <div class="col"> 
+              <div class="row"> 
+                <div class="col-lg-4">
+                  <div class="descripcion">
+                    <p class="purp-text">DEPARTAMENTO TIPO B1</p>
+                    <p class="desde">DESDE <span>2.718 UF</span></p>
+                    <p class="pro-desc">2 DORMITORIOS + 1 BAÑO</p>
+                    <p class="orientacion">Orientación Surponiente</p>
+                    <p class="p-suma">
+                      <span>Superficie interior</span>
+                      <span class="text-right">44,92 m<sup>2</sup></span>
+                    </p>
+                    <p class="p-suma">
+                      <span>Superficie terraza</span>
+                      <span class="text-right">12,69 m<sup>2</sup></span>
+                    </p>
+                    <div class="hr-purp"></div>
+                    <p class="p-suma total">
+                      <span class="stotal">Superficie total</span>
+                      <span class="text-right">57,61 m<sup>2</sup></span>
+                    </p>
+                    <div class="btns_promo">               
+                      <div class="btndepa blue">
+                        <a onclick="ga('send', 'event', 'btn', 'click', 'btn_mas_info_proyecto');" href="#modal_formulario" data-toggle="modal">
+                         Solicita más información    
+                        </a>                 
+                      </div>                                                
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-8">
+                  <div class="contplano">
+                   <img class="plano" src="assets/images/proyectos/tipo_b1.jpg" alt="ICALMA - DEPARTAMENTO TIPO B1">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- FIN PROYECTO 2 -->
+      <!-- PROYECTO 3 -->
+      <div class="proyecto pro3 proOff">
+        <div class="container">
+          <div class="row">
+            <div class="col"> 
+              <div class="row">
+                <div class="col-lg-4">
+                  <div class="descripcion">
+                    <p class="purp-text">DEPARTAMENTO TIPO B2</p>
+                    <p class="desde">DESDE <span>2.735 UF</span></p>
+                    <p class="pro-desc">2 DORMITORIOS + 1 BAÑO</p>
+                    <p class="orientacion">Orientación Nororiente</p>
+                    <p class="p-suma">
+                      <span>Superficie interior</span>
+                      <span class="text-right">44,75 m<sup>2</sup></span>
+                    </p>
+                    <p class="p-suma">
+                      <span>Superficie terraza</span>
+                      <span class="text-right">10,48 m<sup>2</sup></span>
+                    </p>
+                    <div class="hr-purp"></div>
+                    <p class="p-suma total">
+                      <span class="stotal">Superficie total</span>
+                      <span class="text-right">55,23 m<sup>2</sup></span>
+                    </p>
+                    <div class="btns_promo">               
+                      <div class="btndepa blue">
+                        <a onclick="ga('send', 'event', 'btn', 'click', 'btn_mas_info_proyecto');" href="#modal_formulario" data-toggle="modal">
+                         Solicita más información    
+                        </a>                 
+                      </div>                                                
+                    </div>
+                  </div>
+                </div>               
+                <div class="col-lg-8">
+                  <div class="contplano">
+                   <img class="plano" src="assets/images/proyectos/tipo_b2.jpg" alt="ICALMA - DEPARTAMENTO TIPO B2">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- FIN PROYECTO 3 -->
+      <!-- PROYECTO 4 -->
+      <div class="proyecto pro4 proOff">
+        <div class="container">
+          <div class="row">
+            <div class="col"> 
+              <div class="row">
+                <div class="col-lg-4">
+                  <div class="descripcion">
+                    <p class="purp-text">DEPARTAMENTO TIPO C1</p>
+                    <p class="desde">DESDE <span>3.805 UF</span></p>
+                    <p class="pro-desc">2 DORMITORIOS + 2 BAÑOS</p>
+                    <p class="orientacion">Orientación Nororiente</p>
+                    <p class="p-suma">
+                      <span>Superficie interior</span>
+                      <span class="text-right">54,94 m<sup>2</sup></span>
+                    </p>
+                    <p class="p-suma">
+                      <span>Superficie terraza</span>
+                      <span class="text-right">15,41 m<sup>2</sup></span>
+                    </p>
+                    <div class="hr-purp"></div>
+                    <p class="p-suma total">
+                      <span class="stotal">Superficie total</span>
+                      <span class="text-right">70,35 m<sup>2</sup></span>
+                    </p>
+                    <div class="btns_promo">               
+                      <div class="btndepa blue">
+                        <a onclick="ga('send', 'event', 'btn', 'click', 'btn_mas_info_proyecto');" href="#modal_formulario" data-toggle="modal">
+                         Solicita más información    
+                        </a>                 
+                      </div>                                                
+                    </div>
+                  </div>
+                </div>               
+                <div class="col-lg-8">
+                  <div class="contplano">
+                   <img class="plano" src="assets/images/proyectos/tipo_c1.jpg" alt="ICALMA - DEPARTAMENTO TIPO C1">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- FIN PROYECTO 4 -->
+      <!-- PROYECTO 5 -->
+      <div class="proyecto pro5 proOff">
+        <div class="container">
+          <div class="row">
+            <div class="col"> 
+              <div class="row">
+                <div class="col-lg-4">
+                  <div class="descripcion">
+                    <p class="purp-text">DEPARTAMENTO TIPO C3</p>
+                    <p class="desde">DESDE <span>3.845 UF</span></p>
+                    <p class="pro-desc">2 DORMITORIOS + 2 BAÑOS</p>
+                    <p class="orientacion">Orientación Surponiente</p>
+                    <p class="p-suma">
+                      <span>Superficie interior</span>
+                      <span class="text-right">54,23 m<sup>2</sup></span>
+                    </p>
+                    <p class="p-suma">
+                      <span>Superficie terraza</span>
+                      <span class="text-right">14,76 m<sup>2</sup></span>
+                    </p>
+                    <div class="hr-purp"></div>
+                    <p class="p-suma total">
+                      <span class="stotal">Superficie total</span>
+                      <span class="text-right">70,99 m<sup>2</sup></span>
+                    </p>
+                    <div class="btns_promo">               
+                      <div class="btndepa blue">
+                        <a onclick="ga('send', 'event', 'btn', 'click', 'btn_mas_info_proyecto');" href="#modal_formulario" data-toggle="modal">
+                         Solicita más información    
+                        </a>                 
+                      </div>                                                
+                    </div>
+                  </div>
+                </div>               
+                <div class="col-lg-8">
+                  <div class="contplano">
+                   <img class="plano" src="assets/images/proyectos/tipo_c3.jpg" alt="ICALMA - DEPARTAMENTO TIPO C3">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- FIN PROYECTO 5 -->
+      <!-- PROYECTO 6 -->
+      <div class="proyecto pro6 proOff">
+        <div class="container">
+          <div class="row">
+            <div class="col"> 
+              <div class="row">
+                <div class="col-lg-4">
+                  <div class="descripcion">
+                    <p class="purp-text">DEPARTAMENTO TIPO D (TIPO MARIPOSA)</p>
+                    <p class="desde">DESDE <span>3.208 UF</span></p>
+                    <p class="pro-desc">2x (2 DORMITORIOS + 2 BAÑOS)</p>
+                    <p class="orientacion">Orientación Surponiente</p>
+                    <p class="p-suma">
+                      <span>Superficie interior</span>
+                      <span class="text-right">47,27 m<sup>2</sup></span>
+                    </p>
+                    <p class="p-suma">
+                      <span>Superficie terraza</span>
+                      <span class="text-right">11,6 m<sup>2</sup></span>
+                    </p>
+                    <div class="hr-purp"></div>
+                    <p class="p-suma total">
+                      <span class="stotal">Superficie total</span>
+                      <span class="text-right">58,87 m<sup>2</sup></span>
+                    </p>
+                    <div class="btns_promo">               
+                      <div class="btndepa blue">
+                        <a onclick="ga('send', 'event', 'btn', 'click', 'btn_mas_info_proyecto');" href="#modal_formulario" data-toggle="modal">
+                         Solicita más información    
+                        </a>                 
+                      </div>                                                
+                    </div>
+                  </div>
+                </div>               
+                <div class="col-lg-8">
+                  <div class="contplano">
+                   <img class="plano" src="assets/images/proyectos/tipo_d.jpg" alt="ICALMA - DEPARTAMENTO TIPO D (TIPO MARIPOSA)">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- FIN PROYECTO 6 -->
+      <!-- PROYECTO 7 -->
+      <div class="proyecto pro7 proOff">
+        <div class="container">
+          <div class="row">
+            <div class="col"> 
+              <div class="row">
+                <div class="col-lg-4">
+                  <div class="descripcion">
+                    <p class="purp-text">DEPARTAMENTO TIPO E1</p>
+                    <p class="desde">DESDE <span>4.443 UF</span></p>
+                    <p class="pro-desc">3 DORMITORIOS + 2 BAÑOS</p>
+                    <p class="orientacion">Orientación Nororiente</p>
+                    <p class="p-suma">
+                      <span>Superficie interior</span>
+                      <span class="text-right">63,35 m<sup>2</sup></span>
+                    </p>
+                    <p class="p-suma">
+                      <span>Superficie terraza</span>
+                      <span class="text-right">17,78 m<sup>2</sup></span>
+                    </p>
+                    <div class="hr-purp"></div>
+                    <p class="p-suma total">
+                      <span class="stotal">Superficie total</span>
+                      <span class="text-right">81,13 m<sup>2</sup></span>
+                    </p>
+                    <div class="btns_promo">               
+                      <div class="btndepa blue">
+                        <a onclick="ga('send', 'event', 'btn', 'click', 'btn_mas_info_proyecto');" href="#modal_formulario" data-toggle="modal">
+                         Solicita más información    
+                        </a>                 
+                      </div>                                                
+                    </div>
+                  </div>
+                </div>               
+                <div class="col-lg-8">
+                  <div class="contplano">
+                   <img class="plano" src="assets/images/proyectos/tipo_e.jpg" alt="ICALMA - DEPARTAMENTO TIPO E1">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- FIN PROYECTO 6 -->
+    </section> 
---
+++ icalma2/exequiel/galeria.php
@@ -0,0 +1,131 @@
+
+	<div class="contmosaico">
+		<div class="tercio">
+			<div class="overcont mb_15">
+				<div class="box-mosa">		
+					<a class="btn_galeria" data-fancybox="galeriapro" href="assets/images/galeria/cam_ga1.jpg">
+<!-- 						<div class="texto-mosa">
+							<img src="assets/images/play.png" alt="btn play">
+							<p class="mosa_e">Edificio Macul 3401</p>
+							<p class="mosa_t">Tour Virtual</p>
+							<p class="mosa_360">360º</p>
+						</div>	 -->						
+						<div class="thumb thumb1">
+							<div class="shadow"></div>
+						</div>						
+					</a>
+				</div>
+			</div>
+		</div>
+		<!-- barra hermosa -->
+		<div class="bar bar-m view-mob">
+			<div class="triangulito"></div>
+			<p>
+				Elige disfrutar un espacio diseñado para ti, con salas de diversión, piscina, juegos infantiles, quinchos, gimnasio, salas
+				de estudio, y conectado a un barrio residencial con vida, cultura y panoramas. Cerca de una futura estación de metro,
+				supermercados, autopista, carabineros y mucho más. <strong>¡Descubre una nueva forma de vivir con calidad!</strong>
+			</p>
+		</div>
+		<!-- fin barra hermosa -->
+		<div class="cuarto_tercio">
+			<div class="overcont ml_mb_15">
+				<div class="box-mosa">
+					<a class="btn_galeria" data-fancybox="galeriapro" href="assets/images/galeria/cam_ga2.jpg">
+<!-- 						<div class="textover">
+							<p class="vermas">
+								<img src="assets/images/whitearrow.png" alt="icon arrow white">Ver más <br>
+								fotografías
+							</p>
+						</div>	 -->					
+						<div class="thumb thumb2">
+							<div class="shadow"></div>
+						</div>
+					</a>
+				</div>	
+			</div>					
+		</div>
+		<div class="cuarto">
+			<div class="overcont mr_15 mr_m_5">		
+				<div class="box-mosa">
+					<a class="btn_galeria" data-fancybox="galeriapro" href="assets/images/galeria/cam_ga3.jpg">
+<!-- 						<div class="texto-mosa">
+							<img src="assets/images/play.png" alt="btn play">
+							<p class="mosa_t">KITCHENETTE</p>
+							<p class="mosa_e">Un espacio para compartir y crear. Conoce sus detalles.</p>
+						</div> -->							
+						<div class="thumb thumb3">
+							<div class="shadow"></div>
+						</div>	
+					</a>					
+				</div>	
+			</div>					
+		</div>
+		<div class="cuarto">
+			<div class="overcont mr_15 ml_m_5">
+				<div class="box-mosa">
+					<a class="btn_galeria" data-fancybox="galeriapro" href="assets/images/galeria/cam_ga4.jpg">	
+<!-- 						<div class="texto-mosa">
+							<img src="assets/images/play.png" alt="btn play">
+							<p class="mosa_t">TERRAZA</p>
+							<p class="mosa_e">Una vista a tu barrio y un lugar para relajarse.</p>
+						</div>	 -->											
+						<div class="thumb thumb4">
+							<div class="shadow"></div>
+						</div>		
+					</a>						
+				</div>													
+			</div>
+		</div>
+		<div class="cuarto">
+			<div class="overcont mr_15 mr_m_5">
+				<div class="box-mosa">
+					<a class="btn_galeria" data-fancybox="galeriapro" href="assets/images/galeria/cam_ga5.jpg">
+<!-- 						<div class="texto-mosa">
+							<img src="assets/images/play.png" alt="btn play">
+							<p class="mosa_t">ECO-FRIENDLY</p>
+							<p class="mosa_e">Ahorra hasta un 30% de energía con nuestro sistema precalificado.</p>
+						</div>	 -->						
+						<div class="thumb thumb5">
+							<div class="shadow"></div>
+						</div>	
+					</a>						
+				</div>
+			</div>
+		</div>
+		<div class="cuarto">
+			<div class="overcont ml_m_5">
+				<div class="box-mosa">
+					<a class="btn_galeria" data-fancybox="galeriapro" href="assets/images/galeria/cam_ga6.jpg">
+<!-- 						<div class="texto-mosa">
+							<img src="assets/images/play.png" alt="btn play">
+							<p class="mosa_t">ZONAS DE RECICLAJE</p>
+							<p class="mosa_e">Creamos puntos verdes para un máximo cuidado de la comunidad y el planeta.</p>
+						</div>	 -->						
+						<div class="thumb thumb6">
+							<div class="shadow"></div>
+						</div>							
+					</a>	
+				</div>							
+			</div>
+		</div>
+	</div>
+	<!-- galeria invisible -->
+	<div class="d-none">
+		<a data-fancybox="galeriapro" href="assets/images/galeria/cam_ga7.jpg"><img src="assets/images/galeria/cam_ga7.jpg"></a>
+		<a data-fancybox="galeriapro" href="assets/images/galeria/cowork_ga1.jpg"><img src="assets/images/galeria/cowork_ga1.jpg"></a>
+		<a data-fancybox="galeriapro" href="assets/images/galeria/patio_trasero1.jpg"><img src="assets/images/galeria/patio_trasero1.jpg"></a>		
+		<a data-fancybox="galeriapro" href="assets/images/galeria/piscina.jpg"><img src="assets/images/galeria/piscina.jpg"></a>		
+		<a data-fancybox="galeriapro" href="assets/images/galeria/quincho.jpg"><img src="assets/images/galeria/quincho.jpg"></a>		
+		<a data-fancybox="galeriapro" href="assets/images/galeria/sala_ninos.jpg"><img src="assets/images/galeria/sala_ninos.jpg"></a>		
+	</div>
+	<!-- fin galeria invisble -->
+	<!-- barra hermosa -->
+	<div class="bar view-desktop">
+		<div class="triangulito"></div>
+		<p>
+			Icalma Exequiel cuenta con todo lo necesario para vivir con calidad: salas multiuso para niños y adultos, coworking, quincho, piscina, gimnasio; en la mejor ubicación de La Florida, <strong>cerca de todo lo que te gusta: malls, cines, restaurantes y más.</strong>
+		</p>
+	</div>
+	<!-- fin barra hermosa -->
+
+</div>
